104a7fdb9d0b9a7db93497783ddf8814cfecc081
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-06-02 16:50:40 -0600

    REmoved Reduncy of Draw Events

--- src/Services/TurnService.js
+++ src/Services/TurnService.js
@@ -1,9 +1,9 @@
 import { AppState } from '../AppState'
 import { logger } from '../utils/Logger'
 import { dolService } from './DOLService'
-import { eventService } from './EventService'
 import { goodFortuneService } from './GoodFortuneService'
 import { optionToBuyService } from './OptionToBuyService'
+import $ from 'jquery'
 
 class TurnService {
   endTurn() {
@@ -44,30 +44,19 @@ class TurnService {
     const max = Math.floor(100)
     const draw = Math.floor(Math.random() * (max - min + 1) + min)
     // const draw = 1
-    // logger.log('draw is ' + draw)
-    // 20 % chance of DOL..
-    // 40 % chance of uneventfull
-    // 40 % chance of goodFortune
     if (draw <= 20) {
-      this.devilsOwnLuck(draw)
+      dolService.drawFate(draw)
     } else if (draw <= 60) {
-      this.uneventfull()
+      // Uneventfull  20-60
+      AppState.event.title = 'Just another month'
+      AppState.event.greeting = ''
+      AppState.event.text = 'Sometimes nothing happening is okay'
+      AppState.event.closing = ''
+      AppState.event.closeDesc = 'Okay'
+      $('#eventModal').modal('show')
     } else {
-      this.goodFortune(draw)
+      goodFortuneService.drawFate(draw)
     }
-  }
-
-  // Card Draws Below
-  devilsOwnLuck(draw) {
-    dolService.drawFate(draw)
-  }
-
-  uneventfull() {
-    eventService.uneventfull()
-  }
-
-  goodFortune(draw) {
-    goodFortuneService.drawFate(draw)
   }
 }
 export const turnService = new TurnService()

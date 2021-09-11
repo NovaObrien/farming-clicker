8fbf8d01db5cbc10cc21675d5a8725e3f27671b7
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-09-10 21:13:48 -0600

    basic structure for harvesting cattle

--- src/Services/CattleService.js
+++ src/Services/CattleService.js
@@ -1,9 +1,33 @@
+import Swal from 'sweetalert2'
+import { AppState } from '../AppState'
+import { saveState } from '../utils/LocalStorage'
+
 class CattleService {
   checkTend() {
 
   }
 
-  harvestCattle() {
+  harvestCattle(owned) {
+    if (owned.active.workers === false && owned.active.home === false) { return }
+    if (owned.harvested === true) { return }
+
+    const season = AppState.time.season
+
+    if (season === 'September' || season === 'October' || season === 'November' || season === 'December') {
+      const total = owned.cattle * owned.quality
+      Swal.fire({
+        title: 'Harvest',
+        text: 'Your Cattle Sold for ' + (total).toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD'
+        })
+      })
+      AppState.character.currency += total
+    }
+    saveState()
+  }
+
+  resetCattleHarvest() {
 
   }
 }

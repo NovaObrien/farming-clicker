7911823f5d093714a1944ff1d8573f780be8d8c7
Nova (Tanner) O'Brien <tannerkobrien@gmail.com>
2021-09-15 20:22:45 -0600

    removed notes

--- src/Services/CattleService.js
+++ src/Services/CattleService.js
@@ -3,8 +3,6 @@ import { AppState } from '../AppState'
 import { saveState } from '../utils/LocalStorage'
 
 class CattleService {
-  // TODO Not sure how I want to do this but cattle should work by not harvesting will reduce the quality of the cattle over the winter months, as well as increase the quantity which also reduces the quality. The higher the population the higher the chance of outbreak of disease... Anyways thoughts to consider as you build this out
-
   harvestCattle(owned) {
     if (owned.active.workers === false && owned.active.home === false) { return }
     if (owned.harvested === true) { return }
